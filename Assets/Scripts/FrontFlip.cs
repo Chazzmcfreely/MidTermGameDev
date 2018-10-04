ec5881f33efccae180d65d8827880f477e7b6fdd
Charlie Hattabaugh <cwh360@nyu.edu>
2018-10-04 11:06:35 -0400

    sorta working front flips

--- Assets/Scripts/FrontFlip.cs
+++ Assets/Scripts/FrontFlip.cs
@@ -5,27 +5,36 @@ using UnityEngine;
 public class FrontFlip : MonoBehaviour
 {
 
-	private float timer = 0f;
+	public float timer = 0f;
+	public float timerIncrease = 0f;
 	public float rotationSpeed = 0f;
-	
+
+	private Vector3 flipRotation;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		timer = 0f;
+
+		flipRotation = transform.eulerAngles;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		if(Input.GetKeyDown(KeyCode.Space))
 		{
-			timer = 10f;
+			timer += timerIncrease;
 		
-			Debug.Log("FLIP ME");
+			Debug.Log("start flip");
 		}
 		
 		if (timer > 0)
 		{
-			transform.eulerAngles += new Vector3(rotationSpeed, 0f, 0f);
+			Debug.Log(transform.eulerAngles);
+			flipRotation += new Vector3(rotationSpeed, 0f, 0f);
+			transform.eulerAngles = flipRotation;
 			timer -= 1 * Time.deltaTime;
+			Debug.Log("Trying to flip");
 		}
 		
 	}
