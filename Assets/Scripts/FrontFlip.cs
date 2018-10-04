f90953fffc96da3bc1bfd896db8aba043d596ce8
Charlie Hattabaugh <cwh360@nyu.edu>
2018-10-04 10:31:31 -0400

    This is my first commit for real

---
+++ Assets/Scripts/FrontFlip.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontFlip : MonoBehaviour
+{
+
+	private float timer = 0f;
+	public float rotationSpeed = 0f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			timer = 10f;
+		
+			Debug.Log("FLIP ME");
+		}
+		
+		if (timer > 0)
+		{
+			transform.eulerAngles += new Vector3(rotationSpeed, 0f, 0f);
+			timer -= 1 * Time.deltaTime;
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	public float moveSpeed = 4.0f;
+	public float rotationSpeed = 4.0f;
+	private Vector3 playerPos;
+	//private HintScript HintScript;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		//HintScript = GameObject.FindObjectOfType<HintScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(0f, 0f, moveSpeed);
+		}
+		
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.eulerAngles += new Vector3(0f, -rotationSpeed, 0f);
+		}
+		
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.eulerAngles += new Vector3(0f, rotationSpeed, 0f);
+		}
+
+		playerPos = transform.position;
+		//HintScript.checkPosition(playerPos);
+	}
+}
